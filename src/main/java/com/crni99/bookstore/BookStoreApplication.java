900800855f02bfa3f816dd40bb5015ec099e4cbc
Sumit kumar <sumitkumar808393@gmail.com>
2025-11-04 22:12:42 +0530

    Code

--- src/main/java/com/crni99/bookstore/BookStoreApplication.java
+++ src/main/java/com/crni99/bookstore/BookStoreApplication.java
@@ -1,6 +1,6 @@
 package com.crni99.bookstore;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication ;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
